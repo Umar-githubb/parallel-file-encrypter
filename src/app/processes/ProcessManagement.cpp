3b456588500c690c28181aeefcb7c6c1400a78c5
Umar-githubb <167896745+Umar-githubb@users.noreply.github.com>
2026-02-07 15:05:27 +0530

    Create ProcessManagement.cpp

---
+++ src/app/processes/ProcessManagement.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "ProcessManagement.hpp"
+#include <unistd.h>
+#include <cstring>
+#include <sys/wait.h>
+#include "../encryptDecrypt/Cryption.hpp"
+
+ProcessManagement::ProcessManagement() {}
+
+bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task) {
+    taskQueue.push(std::move(task));
+    int pid = fork();
+    if(pid < 0){
+        return false;
+    }else if(pid > 0){
+        std::cout<<"Entering the parent process"<<std::endl;
+    }else{
+        std::cout<<"Entering the child process"<<std::endl;
+        executeTasks();
+        std::cout<<"Exiting the child process"<<std::endl;
+    }
+    return true;
+}
+
+void ProcessManagement::executeTasks() {
+    if(!taskQueue.empty()){
+        std::unique_ptr<Task> task = std::move(taskQueue.front());
+        taskQueue.pop();
+        std::cout<<"Executing task: "<<task->toString()<<std::endl;
+        executeCryption(task->toString());
+    }
+}
